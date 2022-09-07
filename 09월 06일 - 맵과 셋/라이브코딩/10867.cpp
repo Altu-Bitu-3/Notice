e882d912c66111da4cb439eb74aafb62c6acb2ac
junghk0115 <89477748+junghk0115@users.noreply.github.com>
2022-09-08 00:00:28 +0900

    09월 06일 - 맵과 셋

---
+++ 09월 06일 - 맵과 셋/라이브코딩/10867.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n, input;
+    set<int> s;
+
+    //입력
+    cin >> n;
+    while (n--) {
+        cin >> input;
+        s.insert(input); //셋에 삽입
+    }
+    //출력
+    //set<int>::iterator iter;
+    //auto: 초기화 값에 따라 데이터 타입을 자동으로 정해줌
+    for (auto iter = s.begin(); iter != s.end(); iter++) { //set 순회
+        cout << *iter <<' ';
+    }
+    return 0;
+}
