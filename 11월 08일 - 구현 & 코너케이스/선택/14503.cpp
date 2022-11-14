cd6e3f89f195c075d969efd9289fdba06de85fa8
김서연 <86418674+flowersayo@users.noreply.github.com>
2022-11-14 22:21:00 +0900

    Update 14503.cpp

--- 11월 08일 - 구현 & 코너케이스/선택/14503.cpp
+++ 11월 08일 - 구현 & 코너케이스/선택/14503.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-// 북 동 남 서 
-int dr[4] = { -1,0,1,0, };
-int dc[4] = { 0,1 ,0, -1,};
+
 
 int n, m;
 int simulate(vector<vector<int>> board,int r,int c,int d) {
 
-
+	// 북 동 남 서 
+	int dr[4] = { -1,0,1,0, };
+	int dc[4] = { 0,1 ,0, -1,};
 
 	int ans = 0; // 청소하는 칸의 개수
 
@@ -22,15 +22,15 @@ int simulate(vector<vector<int>> board,int r,int c,int d) {
 			board[r][c] = 2; //현재 위치 청소
 			ans++;
 		}
-	
-		int i;
+
+		int step;
 		//왼쪽 방향에 아직 청소하지 않은 공간이 남아있는지
-		for ( i = 0; i < 4; i++) {
-	
+		for ( step = 0; step < 4; step++) {
+
 			d = (d + 3) % 4;//현재 방향의 왼쪽 좌표
 			int nr = r + dr[d];
 			int nc = c + dc[d];
-	
+
 			if (!board[nr][nc]) { // 청소되지 않은 곳이라면
 				// 그 방향으로 회전하고 한칸을 전진
 				r = nr;
@@ -40,14 +40,12 @@ int simulate(vector<vector<int>> board,int r,int c,int d) {
 
 		}
 
-		if (i == 4) { //네 방향 모두 벽이거나 청소가 되어있는 경우
+		if (step == 4) { //네 방향 모두 벽이거나 청소가 되어있는 경우
 
 			//바라 보는 방향을 유지한채로 한 칸 후진
-			int back = (d + 2) % 4;
+			int br = r - dr[d]; 
+			int bc = c - dc[d]; 
 
-			int br = r + dr[back]; 
-			int bc = c + dc[back]; 
-			
 			if (board[br][bc] == 1) { //뒤쪽 방향이 벽인 경우 작동 멈춤 
 				break;
 			}
@@ -56,9 +54,9 @@ int simulate(vector<vector<int>> board,int r,int c,int d) {
 			r = br;
 			c = bc;
 		}
-	
+
 	}
-	
+
 	return ans;
 
 }
